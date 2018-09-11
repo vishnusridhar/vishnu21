0f05e545a672b15c18f4f8f5761254b1b4943e27
vishnusridhar <43023443+vishnusridhar@users.noreply.github.com>
2018-09-11 12:24:40 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,8 @@
+a=input("enter the value of a")
+b=input("enter the value of b")
+if(a>b):
+	print(a,"is greater than ",b)
+elif(a==b)
+    print(a,"is equal to b",b)
+elif(b>a)
+    print(b,"is greater than ",a)
