972bf4074686144ff1fcf12e9d1a62f7b6135a74
vishnusridhar <43023443+vishnusridhar@users.noreply.github.com>
2018-09-20 10:29:28 +0530

    Add files via upload

---
+++ lab20.py
@@ -0,0 +1,4 @@
+
+for a in range(1,11):
+	for i in range(1,21):
+		print(a,"*",i,"=",a*i)
---
+++ marks.py
@@ -0,0 +1,15 @@
+a=int(input("enter the marks:"))
+if(a>=90):
+	print("A+")
+elif(a>=80):
+	print("A")
+elif(a>=70):
+	print("B+")
+elif(a>=65):
+	print("B")
+elif(a>=45):
+	print("C+")
+elif(a>=30):
+	print("C")
+else:
+	print("fail")
