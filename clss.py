59f4e733fda8d895b72c3e414f81d33874fe89f3
vishnusridhar <43023443+vishnusridhar@users.noreply.github.com>
2018-11-22 09:37:40 +0530

    Add files via upload

---
+++ clss.py
@@ -0,0 +1,9 @@
+class abc:
+	x=5
+	y=7
+	def add(ob):
+		c=ob.x+ob.y
+		print(c)
+p=abc()
+print(p.x,p.y)
+p.add()
---
+++ clss2.py
@@ -0,0 +1,9 @@
+#f=open("dl2.txt","a")
+#f.write("python programing")
+#f=open("dl2.txt","r")
+#print(f.read())
+
+f=open("dl2.txt","w")
+f.write("bye")
+f=open("dl2.txt","r")
+print(f.read())
